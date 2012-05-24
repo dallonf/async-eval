8c39791e19a9e1d7cd6dd92038e0b32d1ab5b2f8
DallonF <dallonfdev@gmail.com>
2012-05-24 14:05:52 -0700

    Added more robust flow control

--- test/api.test.js
+++ test/api.test.js
@@ -5,6 +5,10 @@ var wait = function(callback) {
   process.nextTick(callback);
 };
 
+function funcToString(func) {
+  return "(" + func.toString() + ")()";
+}
+
 describe('asyncEval', function() {
   it('should run the callback', function(done) {
     asyncEval('var x = 5;', function() {
@@ -22,17 +26,13 @@ describe('asyncEval', function() {
 
   it('should have access to sandbox objects', function(done) {
     var me = {foo: 'bar'};
-    asyncEval('me.baz = me.foo;', { sandbox: {me: me} }, function() {
+    asyncEval('me.baz = me.foo;', { context: {me: me} }, function() {
       expect(me.baz).to.equal('bar');
       done();
     });
   });
 
   it('should wait for an async function', function(done) {
-    function test() {
-      
-    }
-
     var self = {};
 
     asyncEval("var self = this; wait( function() { this.x = 5; } );"
@@ -40,5 +40,83 @@ describe('asyncEval', function() {
       expect(self.x).to.equal(5);
       done();
     })
+  });
+
+  it('should return a syntax error', function(done) {
+    asyncEval("x = 3.xxxx;", function(err) {
+      expect(err).to.exist;
+      done();
+    });
+  });
+
+  it('should return a runtime error', function(done) {
+    asyncEval("var x = undefined; \n x.toString();", function(err) {
+      expect(err).to.exist;
+      done();
+    });
+  });
+
+  it('should not continue the event after cancel is called', function(done) {
+    var test = function() {
+      setTimeout(function() {
+        this.x = 5;
+      }, 10);
+      cancel();
+    };
+
+    var self = {};
+
+    asyncEval(funcToString(test), {this: self, asyncFunctions: { setTimeout: setTimeout }}, function(err) {
+      setTimeout(function() {
+        expect(this.x).to.not.exist;
+        done();
+      }, 100);
+    });
+  });
+
+  it('should return a runtime error from a callback', function(done) {
+
+    var test = function() {
+      var foo = undefined;
+      wait(function() {
+        foo.toString();
+      });
+    };
+
+    asyncEval(funcToString(test), {asyncFunctions: {wait: wait}}, function(err) {
+      expect(err).to.exist;
+      done();
+    });
+  });
+
+  it('should support namespaced async functions', function(done) {
+
+    var async = {
+      wait: {
+        ten: function(callback) {
+          setTimeout(callback, 10);
+        }
+        , fifty: function(callback) {
+          setTimeout(callback, 50);
+        }
+      }
+    };
+
+    var test = function() {
+      wait.ten(function() {
+        this.x += 10;
+        wait.fifty(function() {
+          this.x += 50;
+        })
+      });
+    };
+
+    var self = {};
+
+    asyncEval(funcToString(test), {asyncFunctions: async, this: self}, function(err) {
+      expect(self.x).to.equal(60);
+      done(err);
+    });
+
   });
 });
